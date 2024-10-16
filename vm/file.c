7e9282e3569e5f5333a3e1adfd9cf843521c03b3
alswo1212 <minjagot@naver.com>
2024-10-16 11:58:30 +0000

    mmap-read pass

--- vm/file.c
+++ vm/file.c
@@ -5,6 +5,7 @@
 #include "userprog/process.h"
 #include "filesys/file.h"
 #include "threads/mmu.h"
+#include "userprog/syscall.h"
 
 static bool file_backed_swap_in (struct page *page, void *kva);
 static bool file_backed_swap_out (struct page *page);
@@ -59,12 +60,12 @@ file_backed_destroy (struct page *page) {
 		if(pml4_is_dirty(pml4, page->va))// 페이지가 수정되었으면 디스크에 있는 파일에 반영
 			file_write_at(file_page->file, page->va, file_page->page_read_bytes, file_page->ofs);
 		
-		palloc_free_page(page->frame->kva);
 		pml4_clear_page(pml4, page->va);
-		spt_remove_page(spt, page);
+		hash_delete(&spt->pages, &page->page_elem);
 
 		// TODOTODO : 프레임 테이블에서 프레임 제거해야될거 같음
 		free(page->frame);
+		page->frame = NULL;
 	}
 }
 
@@ -73,8 +74,13 @@ void *
 do_mmap (void *addr, size_t length, int writable,
 		struct file *file, off_t offset) {
 	void *result = addr;
+	size_t file_start_page = addr;
+	size_t file_end_page = addr + length;
+	off_t file_size = file_length(file);
 	while (length > 0){
 		size_t read_bytes = length > PGSIZE ? PGSIZE : length;
+		if(file_size < PGSIZE)
+			read_bytes = file_size;
 		size_t zero_bytes = PGSIZE - read_bytes;
 		struct load_info *aux = malloc(sizeof(struct load_info));
 		aux->file = file;
@@ -82,12 +88,15 @@ do_mmap (void *addr, size_t length, int writable,
 		aux->writable = writable;
 		aux->page_read_bytes = read_bytes;
 		aux->page_zero_bytes = zero_bytes;
+		aux->file_start_page = file_start_page;
+		aux->file_end_page = file_end_page;
+
 		if(!vm_alloc_page_with_initializer(VM_FILE, addr, writable, lazy_load_file, aux))
 			return NULL;
 		
-		offset += read_bytes;
-		length -= read_bytes;
-		addr += read_bytes;
+		offset += PGSIZE;
+		length -= PGSIZE;
+		addr += PGSIZE;
 		// TODO : 중간에 실패하면 기존에 만들었던 aux를 free해야되지 않을까?
 	}
 	return result;
@@ -109,8 +118,7 @@ do_munmap (void *addr) {
 		if(p == NULL)
 			return;
 
-		file_backed_destroy(p);
-		free(p);
+		spt_remove_page(spt, p);
 	}
 }
 
@@ -118,10 +126,13 @@ static bool lazy_load_file(struct page *page, void *aux){
 	struct load_info *info = (struct load_info *)aux;
 	uint8_t *kpage = page->frame->kva;
 
+	lock_acquire(&filesys_lock);
 	file_seek(info->file, info->ofs);
-	if (info->file != NULL && file_read(info->file, kpage, info->page_read_bytes) != (int)info->page_read_bytes){
+	int read_bytes = file_read(info->file, kpage, info->page_read_bytes);
+	if (info->file != NULL && read_bytes != (int)info->page_read_bytes){
 		palloc_free_page(kpage);
 		free(aux);
+		lock_release(&filesys_lock);
 		return false;
 	}
 	memset(kpage + info->page_read_bytes, 0, info->page_zero_bytes);
@@ -134,6 +145,7 @@ static bool lazy_load_file(struct page *page, void *aux){
 	page->file.ofs = info->ofs;
 
 	free(aux);
+	lock_release(&filesys_lock);
 
 	return true;
 }
